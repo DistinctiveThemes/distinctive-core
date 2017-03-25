2ce8e1d9fb18ab59d8674a4e66e43f48d3c58b2e
Danny Jones <danny@distinctivethemes.com>
2017-03-25 04:23:26 +0000

    check in

--- page_builder_blocks/buzzpress/page_builder_init.php
+++ page_builder_blocks/buzzpress/page_builder_init.php
@@ -12,4 +12,12 @@ require_once( DISTINCTIVE_CORE_PATH . 'page_builder_blocks/buzzpress/media_grid.
 require_once( DISTINCTIVE_CORE_PATH . 'page_builder_blocks/buzzpress/simple_button.php' );
 require_once( DISTINCTIVE_CORE_PATH . 'page_builder_blocks/buzzpress/revslider.php' );
 
+if ( !function_exists('is_plugin_active') ) {
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+}
+
+if (is_plugin_active( 'user-submitted-posts/user-submitted-posts.php' )) {
+	require_once( DISTINCTIVE_CORE_PATH . 'page_builder_blocks/buzzpress/user_submitted_blog_feed.php' );
+}
+
 ?>
